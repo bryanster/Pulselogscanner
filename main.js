4a170cd19c06b82559bd0a631a0168ed3fa36944
bryanster <bnet1998@gmail.com>
2021-05-19 22:09:09 +0200

    update

--- main.js
+++ main.js
@@ -7,11 +7,6 @@ if(path == "-h"){
     console.log("to use this applicatie the first argument should be a readable file \n to create a file per country use the -f flag")
 }
 else{
-
-if(folder == "-f"){
-    if (!fs.existsSync("./countries")) {
-        fs.mkdirSync("./countries")
-    }
     var r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 
 const data = fs.readFileSync(path[0], "utf8");
@@ -22,29 +17,18 @@ lines.forEach((element) => {
     if(ip !== null){
         var test = (geoip.lookup(ip[0]))
         if(test !== null){
+            if(folder == "-f"){
+                if (!fs.existsSync("./countries")) {
+                    fs.mkdirSync("./countries")
+                }
             fs.appendFileSync(("./countries/" + test.country + ".txt"), ip[0] + "\n")
-            
+            }
+            else{
+                console.log(ip + " = " + test.country)
+            }
+
         }
     }
   
 } )
-}
-else{
-var r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
-
-const data = fs.readFileSync(path[0], "utf8");
-var lines = data.split("\n");
-
-lines.forEach((element) => {
-    ip = element.match(r)
-    if(ip !== null){
-        var test = (geoip.lookup(ip[0]))
-        if(test !== null){
-            // fs.appendFileSync(("./countries/" + test.country + ".txt"), ip[0] + "\n")
-            console.log(ip + " = " + test.country)
-        }
-    }
-  
-} )
-}
 }
